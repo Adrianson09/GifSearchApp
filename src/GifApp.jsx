da4bf283b51032c6044826585524dba1f4f8b29f
Adrian Guerrero <adrianson@outlook.com>
2023-04-18 10:46:35 -0600

    add desc commit

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -16,7 +16,8 @@ export const GifApp = () => {
   return (
     <>
         
-        <h1>Gif App</h1>
+        <h1>Gif Search App</h1>
+        <p>¡Bienvenido a nuestra aplicación de búsqueda de GIFs! Aquí encontrarás una amplia selección de GIFs divertidos y emocionantes para compartir con tus amigos y familiares en cualquier momento y en cualquier lugar. Desde reacciones hilarantes hasta momentos épicos, nuestra biblioteca de GIFs tiene algo para todos. ¡Empieza a explorar ahora y encuentra el GIF perfecto para expresar cómo te sientes!</p>
         
         <AddCategory 
             onNewCategory={ event => onAddCategory(event)}
